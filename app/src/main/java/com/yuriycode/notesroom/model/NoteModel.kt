453d03651f6b54bf049a132a93fcb39615148756
Yuriy Kravets <97830497+yuriycode@users.noreply.github.com>
2022-08-11 18:06:44 +0300

    Working with NoteModel and NoteAdapter.

--- app/src/main/java/com/yuriycode/notesroom/model/NoteModel.kt
+++ app/src/main/java/com/yuriycode/notesroom/model/NoteModel.kt
@@ -1,4 +1,17 @@
 package com.yuriycode.notesroom.model
 
-class NoteModel {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+class NoteModel (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+
+    @ColumnInfo
+    var title: String = "",
+
+    @ColumnInfo
+    var description: String = ""
+)
