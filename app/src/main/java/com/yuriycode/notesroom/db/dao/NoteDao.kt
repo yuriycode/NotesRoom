3c865f5061bdb05391a432b9e8ebb56f52d6d756
Yuriy Kravets <97830497+yuriycode@users.noreply.github.com>
2022-08-12 10:22:27 +0300

    Working with DataBase, coding our Interfaces and implement them in classes. Then wy synchronized our abstract class with DataBase using annotation.

--- app/src/main/java/com/yuriycode/notesroom/db/dao/NoteDao.kt
+++ app/src/main/java/com/yuriycode/notesroom/db/dao/NoteDao.kt
@@ -1,4 +1,20 @@
 package com.yuriycode.notesroom.db.dao
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.yuriycode.notesroom.model.NoteModel
+
+@Dao
 interface NoteDao {
+
+    @Insert
+    suspend fun insert(noteModel: NoteModel)
+
+    @Insert
+    suspend fun delete(noteModel: NoteModel)
+
+    @Query("SELECT * from note_table")
+    fun getAllNotes(): LiveData<List<NoteModel>>
 }
